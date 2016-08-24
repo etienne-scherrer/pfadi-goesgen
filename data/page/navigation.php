636a93bb1b5d5598e57287e1f39eee99ea696bcf
Etoma <etienne.scherrer@hotmail.com>
2016-08-24 22:13:44 +0200

    Changed: Hide "Sommerfest" Tab in the Navigation

--- data/page/navigation.php
+++ data/page/navigation.php
@@ -6,8 +6,7 @@
             <li id="navigation-portrait" class="<?= $activePage === 'portrait' ? 'active' : 'inactive'?>"><a title="Portrait" href="portrait.php"><span>Portrait</span></a></li>
             <li id="navigation-pfadiheim" class="<?= $activePage === 'pfadiheim' ? 'active' : 'inactive'?>"><a title="Pfadiheim" href="pfadiheim.php"><span>Pfadiheim</span></a></li>
             <li id="navigation-galerie" class="<?= $activePage === 'galerie' ? 'active' : 'inactive'?>"><a title="Galerie" href="galerie.php"><span>Galerie</span></a></li>
-            <li id="navigation-sommerfest" class="<?= $activePage === 'sommerfest' ? 'active' : 'inactive'?>"><a title="Sommerfest" href="sommerfest.php"><span>Sommerfest</span></a>
-            </li>
+           <!-- <li id="navigation-sommerfest" class="<?= $activePage === 'sommerfest' ? 'active' : 'inactive'?>"><a title="Sommerfest" href="sommerfest.php"><span>Sommerfest</span></a></li> -->
         </ul>
     </div>
 </div>
