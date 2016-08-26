c8008daf386fedb426a02932e285837de7f397be
Etoma <etienne.scherrer@hotmail.com>
2016-08-26 14:05:51 +0200

    Changed: cache the konami-code logo (part 2)

--- data/page/header.php
+++ data/page/header.php
@@ -28,7 +28,7 @@
         keys.push(e.keyCode);
         if (keys.toString().indexOf(konami) >= 0) {
             $(document).unbind('keydown', arguments.callee);
-            $('#logo img').attr('src', 'https://pfadi-goesgen.ch/bilder/logos/Pokeball.png');
+            $('#logo img').attr('src', '../..//bilder/logos/Pokeball.png');
             $('#logo').append('<div id="copyright">By Fuchur</div>');
             $('#copyright').css('margin', '-60px 50px');
         }
